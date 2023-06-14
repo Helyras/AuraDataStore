88e335f94294e09ad0f04210dff135809edc8c30
Zepheria <07emirdeliorman26@gmail.com>
2023-06-15 01:26:17 +0300

    v1.0.2

--- src/ExampleUse.server.lua
+++ src/ExampleUse.server.lua
@@ -5,42 +5,42 @@ local AuraDataStore = require(ServerStorage:WaitForChild("AuraDataStore"))
 AuraDataStore.SaveInStudio = true
 
 local DataTemplate = {
-  Cash = 0
+	Cash = 0
 }
 
 local PlayerDataStore = AuraDataStore.CreateStore("PlayerDataStore", DataTemplate)
 
 Players.PlayerAdded:Connect(function(player)
-  local key = player.UserId
-  local data, reason = PlayerDataStore:GetAsync(key)
+	local key = player.UserId
+	local data, reason = PlayerDataStore:GetAsync(key)
 
-  if not data then
-    player:Kick(reason)
-    return
-  end
+	if not data then
+		player:Kick(reason)
+		return
+	end
 
-  PlayerDataStore:Reconcile(key) -- optional
+	PlayerDataStore:Reconcile(key) -- optional
 
-  local folder = Instance.new("Folder")
-  folder.Name = "leaderstats"
+	local folder = Instance.new("Folder")
+	folder.Name = "leaderstats"
 
-  local cash = Instance.new("IntValue")
-  cash.Name = "Cash"
-  cash.Parent = folder
+	local cash = Instance.new("IntValue")
+	cash.Name = "Cash"
+	cash.Parent = folder
 
-  cash.Value = data.Cash
-  cash.Changed:Connect(function()
-    data.Cash = cash.Value
-  end)
+	cash.Value = data.Cash
+	cash.Changed:Connect(function()
+		data.Cash = cash.Value
+	end)
 
-  folder.Parent = player
+	folder.Parent = player
 end)
 
 Players.PlayerRemoving:Connect(function(player)
-  local key = player.UserId
-  PlayerDataStore:SaveOnLeave(key, {key})
+	local key = player.UserId
+	PlayerDataStore:SaveOnLeave(key, {key})
 end)
 
 AuraDataStore.DataStatus:Connect(function(info, key, name, response, retries, sessionLockCooldown)
-  warn(info)
+	warn(info)
 end)
